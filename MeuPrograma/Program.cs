41e0d2e110003772d98810b11459c0caabae1dc0
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-19 19:00:49 +0000

    Changed some indentations

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bye World!");
+﻿using System ;
+
+    namespace MeuPrograma
+    {
+        class Program
+        {
+        static void Main (string[] args)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Bye World!");
+            }
+        }
+    }
