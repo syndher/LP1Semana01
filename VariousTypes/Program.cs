f586bac788a8a8f38230b07a7b997598f2aedf6f
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-19 19:21:38 +0000

    Added integers to the script

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System ;
+
+    namespace MeuPrograma
+    {
+        /// <summary>
+        /// Chooses the class
+        /// </summary>
+        class Program
+        {
+            /// <summary>
+            /// Starts the script to write various numbers and values on the console
+            /// </summary>
+            /// <param name="args"></param>
+        static void Main (string[] args)
+            {
+                int a = 1;
+                uint b = 0x0F;
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+        }
+    }
