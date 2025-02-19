2bf7feedd0d3d62204519d8bfc2b1046aed084c0
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-19 19:03:44 +0000

    Added /// to Main and Program

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,9 +2,9 @@
 
     namespace MeuPrograma
     {
-        class Program
+        class ///Program
         {
-        static void Main (string[] args)
+        static void ///Main (string[] args)
             {
                 Console.WriteLine("Hello World!");
                 Console.WriteLine("Bye World!");
