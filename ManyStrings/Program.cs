0496e3d591603a5fc010088f9b6a1f749f8ebf45
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-19 19:52:56 +0000

    Added strings with unicode characters

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System ;
+using System.Text;
+
+
+    namespace MeuPrograma
+    {
+        /// <summary>
+        /// Chooses the class
+        /// </summary>
+        class Program
+        {
+            /// <summary>
+            /// Starts the script to write "Hello World!" and
+            /// "Bye World!" on the console
+            /// </summary>
+            /// <param name="args"></param>
+        static void Main (string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                string introducao = "Bom dia \u263C";
+                string fim = "Acabou a a"+'\u00E7'+'\u00E3'+"o";
+                Console.WriteLine(introducao);
+                Console.WriteLine(fim);
+            }
+        }
+    }
