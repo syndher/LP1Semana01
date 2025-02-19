f5ca3f5e7dde7b90bbcd28b78f2f0121f8b98ac1
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-19 20:31:04 +0000

    Converts user input into integer

---
+++ AskForInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System ;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MeuPrograma
+    {
+        /// <summary>
+        /// Chooses the class
+        /// </summary>
+        class Program
+        {
+            /// <summary>
+            /// Starts the script to add two numbers given by the user
+            /// </summary>
+            /// <param name="args"></param>
+        static void Main (string[] args)
+            {
+                Console.WriteLine("Enter an integer:");
+                string x = Console.ReadLine();
+                int y = int.Parse(x);
+
+
+
+            }
+        }
+    }
