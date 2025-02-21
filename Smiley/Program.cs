f3b5fa949ccba237b6eae5c307b91f00d6960bfe
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-02-21 17:30:43 +0000

    Added Encoding and Read line

---
+++ Smiley/Program.cs
@@ -0,0 +1,20 @@
+﻿using System ;
+using System.Text;
+
+    namespace Smiley
+    {
+        class Program
+        {
+
+        static void Main (string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine("Enter an hexadecimal:");
+                string x = Console.ReadLine();
+
+
+            
+                
+            }
+        }
+    }
